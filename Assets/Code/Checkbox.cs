0a466cafdc16f0ad992fbac454ade88a3ae45bf2
Kalion1 <laura.vassal.gd@gmail.com>
2021-10-11 11:31:41 +0200

    Jeu qui marche

--- Assets/Code/Checkbox.cs
+++ Assets/Code/Checkbox.cs
@@ -4,25 +4,19 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Checkbox : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler
+public class Checkbox : MonoBehaviour,IPointerClickHandler
 {
     
-    //définition de l'enum (réutilisable par d'autres)
-    public enum Content
-    {
-        None = 0,
-        PlayerOne = 1,
-        PlayerTwo = 2
-    }
     
-    // Variables de la classe
-    public Content currentContent;
     GameCtrl myGame;
+    public int ID;
+    bool canClick;
     
     // Start is called before the first frame update
     void Start()
     {
         myGame = FindObjectOfType<GameCtrl>();
+        canClick = true;
     }
 
     // Update is called once per frame
@@ -31,25 +25,23 @@ public class Checkbox : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler,
         
     }
 
-    public void HoverMe()
-    {
-        Debug.Log("I'm here", this);
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("Enter me" + name, this);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        Debug.Log("Exit me" + name, this);
-    }
-
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("Click me" + name, this);
-        GetComponent<Image>().sprite = myGame.playerOneCheckbox;
-        currentContent = Content.PlayerOne;
+        if (!canClick) return;
+        if (myGame.currentContent == GameCtrl.Content.Victory || myGame.currentContent == GameCtrl.Content.Defeat) return;
+        canClick = false;
+        
+        if (myGame.currentContent == GameCtrl.Content.PlayerOne)
+        {
+            GetComponent<Image>().sprite = myGame.playerOneCheckbox;
+            //myGame.currentContent = GameCtrl.Content.PlayerTwo;
+        }
+        else if (myGame.currentContent == GameCtrl.Content.PlayerTwo)
+        {
+            GetComponent<Image>().sprite = myGame.playerTwoCheckbox;
+            //myGame.currentContent = GameCtrl.Content.PlayerOne;
+        }
+        myGame.changePlayer(ID);
+        
     }
 }
--- Assets/Code/GameCtrl.cs
+++ Assets/Code/GameCtrl.cs
@@ -7,9 +7,110 @@ public class GameCtrl : MonoBehaviour
     public Sprite playerOneCheckbox;
     public Sprite playerTwoCheckbox;
 
+    //définition de l'enum (réutilisable par d'autres)
+    public enum Content
+    {
+        None = 0,
+        PlayerOne = 1,
+        PlayerTwo = 2,
+
+        Victory = 3,
+        Defeat = 4,
+    }
+
+    // Variables de la classe
+    public Content currentContent;
+    public Content Victorious;
+
+    public List<int> playerOneActions = new List<int>();
+    public List<int> playerTwoActions = new List<int>();
+
     void Start()
     {
-        
+        currentContent = Content.PlayerOne;
     }
+
+    
+
+    //Changement de joueur et Victoire
+    // on attribue un int à chaque checkbox
+    //Check ligne 1 => if (1,2,3) => victory
+    public void changePlayer(int _id)
+    {
+        if (currentContent == Content.PlayerOne)
+        {
+            currentContent = Content.PlayerTwo;
+            playerOneActions.Add(_id);
+        }
+        else if (currentContent == Content.PlayerTwo)
+        {
+            currentContent = Content.PlayerOne;
+            playerTwoActions.Add(_id);
+        }
+        checkVictory(playerOneActions);
+        checkVictory(playerTwoActions);
+    }
+
+    void checkVictory(List<int> _listToCheck)
+    {
+        bool Victory = false;
+        if (_listToCheck.Contains (1) && _listToCheck.Contains (2) && _listToCheck.Contains(3))
+        {
+            Victory = true;
+        }
+
+        else if (_listToCheck.Contains(4) && _listToCheck.Contains(5) && _listToCheck.Contains(6))
+        {
+            Victory = true;
+        }
+
+        else if (_listToCheck.Contains(7) && _listToCheck.Contains(8) && _listToCheck.Contains(9))
+        {
+            Victory = true;
+        }
+        else if (_listToCheck.Contains(1) && _listToCheck.Contains(4) && _listToCheck.Contains(7))
+        {
+            Victory = true;
+        }
+
+        else if (_listToCheck.Contains(2) && _listToCheck.Contains(5) && _listToCheck.Contains(8))
+        {
+            Victory = true;
+        }
+
+        else if (_listToCheck.Contains(3) && _listToCheck.Contains(6) && _listToCheck.Contains(9))
+        {
+            Victory = true;
+        }
+        else if (_listToCheck.Contains(1) && _listToCheck.Contains(5) && _listToCheck.Contains(9))
+        {
+            Victory = true;
+        }
+
+        else if (_listToCheck.Contains(3) && _listToCheck.Contains(5) && _listToCheck.Contains(7))
+        {
+            Victory = true;
+        }
+
+        if (Victory)
+        {
+            declareVictory(_listToCheck);
+        }
+
+    }
+
+    void declareVictory(List<int> _listToCheck)
+    {
+        if (_listToCheck == playerOneActions) Victorious = Content.PlayerOne;
+        if (_listToCheck == playerTwoActions) Victorious = Content.PlayerTwo;
+        currentContent = Content.Victory;
+        Debug.Log("VICTOIRE" + Victorious);
+    }
+
+    void declareDefeat()
+    {
+
+    }
+    
 
 }
